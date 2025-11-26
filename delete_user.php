06eecc1d7e1f6e5f08785bee32cbec1fe4fcf784
md masum prodhania <mdprodhaniamasum@gmail.com>
2025-11-26 13:18:18 +0600

    Refactor delete_user.php for clarity
    Refactor delete user logic to improve readability and structure.

--- delete_user.php
+++ delete_user.php
@@ -1,22 +1,15 @@
 <?php
-// Process delete operation after confirmation
 if(isset($_POST["id"]) && !empty($_POST["id"])){
-    // Include config file
     require_once "config.php";
 
-    // Prepare a delete statement
     $sql = "DELETE FROM users WHERE id = ?";
 
     if($stmt = mysqli_prepare($link, $sql)){
-        // Bind variables to the prepared statement as parameters
         mysqli_stmt_bind_param($stmt, "i", $param_id);
 
-        // Set parameters
         $param_id = trim($_POST["id"]);
 
-        // Attempt to execute the prepared statement
         if(mysqli_stmt_execute($stmt)){
-            // Records deleted successfully. Redirect to landing page
             header("location: admin_dashboard.php");
             exit();
         } else{
@@ -24,15 +17,11 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
         }
     }
 
-    // Close statement
     mysqli_stmt_close($stmt);
 
-    // Close connection
     mysqli_close($link);
 } else{
-    // Check existence of id parameter
     if(empty(trim($_GET["id"]))){
-        // URL doesn't contain id parameter. Redirect to error page
         header("location: error.php");
         exit();
     }
