903ef511c2b76b825580abcf58f457a095c52b16
md masum prodhania <mdprodhaniamasum@gmail.com>
2025-11-26 13:25:43 +0600

    Update read_user.php

--- read_user.php
+++ read_user.php
@@ -1,33 +1,24 @@
 <?php
-// Check existence of id parameter before processing further
 if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
-    // Include config file
     require_once "config.php";
 
-    // Prepare a select statement
     $sql = "SELECT * FROM users WHERE id = ?";
 
     if($stmt = mysqli_prepare($link, $sql)){
-        // Bind variables to the prepared statement as parameters
         mysqli_stmt_bind_param($stmt, "i", $param_id);
 
-        // Set parameters
         $param_id = trim($_GET["id"]);
 
-        // Attempt to execute the prepared statement
         if(mysqli_stmt_execute($stmt)){
             $result = mysqli_stmt_get_result($stmt);
 
             if(mysqli_num_rows($result) == 1){
-                /* Fetch result row as an associative array. Since the result set
-                contains only one row, we don't need to use while loop */
+                
                 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
-                // Retrieve individual field value
                 $username = $row["username"];
                 $created_at = $row["created_at"];
             } else{
-                // URL doesn't contain valid id parameter. Redirect to error page
                 header("location: error.php");
                 exit();
             }
@@ -37,13 +28,10 @@ if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
         }
     }
 
-    // Close statement
     mysqli_stmt_close($stmt);
 
-    // Close connection
     mysqli_close($link);
 } else{
-    // URL doesn't contain id parameter. Redirect to error page
     header("location: error.php");
     exit();
 }
